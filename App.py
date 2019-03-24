fd887036de976a0df3b5de54fa7d69d79445274a
SebastianMOtero <sebastian.otero@live.com.ar>
2019-03-24 08:51:36 -0300

    Contact App

--- App.py
+++ App.py
@@ -30,15 +30,30 @@ def add_contact():
         flash('Contact Added Successfully')
         return redirect(url_for('Index'))
 
-@app.route('/edit')
-def edit_contact():
-    return 'Edit contact'
+@app.route('/edit/<id>')
+def edit_contact(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM contacts WHERE ID = %s', (id))
+    data = cursor.fetchall()
+    return render_template('edit_contact.html', contact = data[0])
+
+@app.route('/update/<id>', methods = ['POST'])
+def update(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute('UPDATE contacts SET fullname = %s, phone = %s, email = %s WHERE id = %s', (fullname, phone, email, id))
+        mysql.connection.commit()
+        flash('Contact Updated Successfully')
+        return redirect(url_for('Index'))
 
 @app.route('/delete/<string:id>')
 def delete_contact(id):
     cursor = mysql.connection.cursor()
     print(id)
-    cursor.execute('DELETE FROM contacts WHERE id = %d', format(id))
+    cursor.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
     mysql.connection.commit()
     flash('Contact Removed Successfully')
     return redirect(url_for('Index'))
